36a11b9c24b0870edec25a2d3c569e903d56756a
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-08 05:14:40 +0100

    docker setup

--- routes/hello.go
+++ routes/hello.go
@@ -1,10 +1,8 @@
 package routes
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"github.com/supabase-community/supabase-go"
 	"log"
 	"os"
 )
@@ -23,16 +21,6 @@ func GetEnv(key string) string {
 
 // Hello handler
 func Hello(c *fiber.Ctx) error {
-
-	client, err := supabase.NewClient(GetEnv("API_URL"), GetEnv("API_KEY"), &supabase.ClientOptions{})
-
-	if err != nil {
-		fmt.Println("cannot initalize client", err)
-	}
-
-	data, count, err := client.From("countries").Select("*", "exact", false).Execute()
-
-	log.Println(data, count)
 
 	return c.SendString("Hello, World!")
 }
