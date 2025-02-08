db0e30348322f24e20376701d8d1f09e86506602
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-08 19:29:16 +0100

    changed api db host to db

--- database/db.go
+++ database/db.go
@@ -15,7 +15,7 @@ func ConnectToDB() {
 	var err error
 	// Replace with your actual connection details
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+		os.Getenv("API_DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	DB, err = sqlx.Connect("postgres", dsn)
 	if err != nil {
